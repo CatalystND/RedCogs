ab51dbfd25f5d8e664a69b1b1efcf7ffc30866f3
Nick Daino <nick@nickdaino.tech>
2026-01-10 22:44:38 -0500

    removed misnamed version of bristol cog

--- bristolConsitions/bristol.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import urllib.request, re
-page = str(urllib.request.urlopen('https://www.bristolmountain.com/conditions/'))
-# print(page.read())
-
-cometRegex = re.compile("Comet Express Quad")
-cometStatus = re.search("Comet Express Quad(.{20})", page)
-print(cometStatus)
